5c51ce36e16df9b56e2c71918d6054ef87e77bf2
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2023-08-23 17:47:13 +0000

    feat: added routes in the user controller

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,10 +4,20 @@ import { LocalStrategy } from './local.strategy';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [LocalStrategy,AuthService],
-  imports: [UsersModule,PassportModule]
+  providers: [LocalStrategy, AuthService, JwtStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWTKEY,
+      signOptions: { expiresIn: process.env.TOKEN_EXPIRATION },
+    }),
+  ],
 })
 export class AuthModule {}
