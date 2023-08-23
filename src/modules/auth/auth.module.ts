daab7c2dff7656b05fe00a751361b14526928c4c
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2023-08-23 13:37:51 +0000

    feat: added auth service and auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.strategy';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [LocalStrategy,AuthService],
+  imports: [UsersModule,PassportModule]
 })
 export class AuthModule {}
