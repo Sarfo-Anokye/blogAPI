f5957de1b4bc7775ee09d4e209a29a2c79d91b7b
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2023-08-28 16:04:00 +0000

    feat: modified the validatePipe

--- src/core/pipe/validate.pipe.ts
+++ src/core/pipe/validate.pipe.ts
@@ -1,23 +1,24 @@
-import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
-import { validate } from 'class-validator';
-import { plainToInstance } from 'class-transformer';
+import {
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+  ValidationPipe,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 
 @Injectable()
-export class ValidationPipe implements PipeTransform<any> {
-  async transform(value: any, { metatype }: ArgumentMetadata) {
-    if (!metatype || !this.toValidate(metatype)) {
-      return value;
+export class ValidateInputPipe extends ValidationPipe {
+  public async transform(value, metadata: ArgumentMetadata) {
+    try {
+      return await super.transform(value, metadata);
+    } catch (e) {
+      if (e instanceof BadRequestException) {
+        throw new UnprocessableEntityException(this.handleError(e));
+      }
     }
-    const object = plainToInstance(metatype, value);
-    const errors = await validate(object);
-    if (errors.length > 0) {
-      throw new BadRequestException('Validation failed');
-    }
-    return value;
   }
 
-  private toValidate(metatype: Function): boolean {
-    const types: Function[] = [String, Boolean, Number, Array, Object];
-    return !types.includes(metatype);
+  private handleError(errors) {
+    return errors.response.message.map((error) => error);
   }
 }
