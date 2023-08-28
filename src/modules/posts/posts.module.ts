b5bcbf44aac317317e1f65196e9b5cd57acf8d81
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2023-08-28 16:43:17 +0000

    feat: added crud operation in the post service

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { postProviders } from './post.provider';
 
 @Module({
-  providers: [PostsService],
+  providers: [PostsService,...postProviders],
   controllers: [PostsController]
 })
 export class PostsModule {}
