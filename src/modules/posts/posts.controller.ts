8774f46cf15f259f2732460a463626109e960ba4
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2023-09-20 10:49:47 +0000

    refactor: typing parameters

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,57 +1,93 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, NotFoundException, UseGuards, Request, ConsoleLogger } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { PostsService } from './posts.service';
-import { Post as PostEntity } from './post.entity';
-import { PostDto } from './dto/post.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Request,
+  UseGuards
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/core/guards/jwt-ayth.guard';
+import { PostDto } from './dto/post.dto';
+import { Post as PostEntity } from './post.entity';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
-    constructor(private readonly postService: PostsService) { }
+  constructor(private readonly postService: PostsService) {}
 
-    @Get()
-    async findAll() {
-        return await this.postService.findAll();
+  @Get()
+  async findAll() {
+    try {
+      return await this.postService.findAll();
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.EXPECTATION_FAILED);
     }
+  }
 
-    @Get(':id')
-    async findOne(@Param('id') id: number): Promise<PostEntity> {
-        const post = await this.postService.findOne(id);
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<PostEntity> {
+    try {
+      const post = await this.postService.findOne(id);
 
-        if (!post) {
-            throw new NotFoundException('This Post doesn\'t exist');
-        }
-
-        return post;
+      if (!post) {
+        throw new NotFoundException("This Post doesn't exist");
+      }
+      return post;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.EXPECTATION_FAILED);
     }
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Post()
-    async create(@Body() post: PostDto, @Request() req): Promise<PostEntity> {
-        return await this.postService.create(post, req.user.id);
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async create(@Body() post: PostDto, @Request() req): Promise<PostEntity> {
+    try {
+      return await this.postService.create(post, req.user.id);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.EXPECTATION_FAILED);
     }
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Put(':id')
-    async update(@Param('id') id: number, @Body() post: PostDto, @Request() req): Promise<PostEntity> {
-        const { numberOfAffectedRows, updatedPost } = await this.postService.update(id, post, req.user.id);
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() post: PostDto,
+    @Request() req,
+  ): Promise<PostEntity> {
+    try {
+      const { numberOfAffectedRows, updatedPost } =
+        await this.postService.update(id, post, req.user.id);
 
-        if (numberOfAffectedRows === 0) {
-            throw new NotFoundException('This Post doesn\'t exist');
-        }
+      if (numberOfAffectedRows === 0) {
+        throw new NotFoundException("This Post doesn't exist");
+      }
 
-        return updatedPost;
+      return updatedPost;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.EXPECTATION_FAILED);
     }
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Delete(':id')
-    async remove(@Param('id') id: number, @Request() req) {
-        const deleted = await this.postService.delete(id, req.user.id);
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async remove(@Param('id') id: number, @Request() req) {
+    try {
+      const deleted = await this.postService.delete(id, req.user.id);
 
-        if (deleted === 0) {
-            throw new NotFoundException('This Post doesn\'t exist');
-        }
+      if (deleted === 0) {
+        throw new NotFoundException("This Post doesn't exist");
+      }
 
-        return 'Successfully deleted';
+      return 'Successfully deleted';
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.EXPECTATION_FAILED);
     }
+  }
 }
